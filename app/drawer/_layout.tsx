d34842506eab496c8cc6c4bb894f0b95e5f14b92
Milton Munoz <livemilton@gmail.com>
2026-08-12 21:17:47 -0500

    feature((HU-02)-HU(HU-02): :sparkles: React native - Drawer - custom drawer

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -1,25 +1,49 @@
 import React from "react";
 import { Drawer } from "expo-router/drawer";
+import { Ionicons } from "@expo/vector-icons";
 
 const DrawerLayout = () => {
-  return (
-    <Drawer>
-      <Drawer.Screen
-        name="user/index" // This is the name of the page and must match the url from root
-        options={{
-          drawerLabel: "User",
-          title: "usuario",
-        }}
-      />
-      <Drawer.Screen
-        name="schedule/index" // This is the name of the page and must match the url from root
-        options={{
-          drawerLabel: "Horario",
-          title: "Horario",
-        }}
-      />
-    </Drawer>
-  );
+    return (
+        <Drawer
+            screenOptions={{
+                overlayColor: "rgba(0,0,0,0.4)",
+                drawerActiveTintColor: "indigo",
+                headerShadowVisible: false,
+                sceneContainerStyle: {
+                    backgroundColor: "white",
+                },
+            }}
+        >
+            <Drawer.Screen
+                name="user/index" // This is the name of the page and must match the url from root
+                options={{
+                    drawerLabel: "User",
+                    title: "usuario",
+                    drawerIcon: ({ color, size }) => {
+                        <Ionicons name="person-circle-outline"
+                            size={size}
+                            color={color}
+
+                        />;
+                    },
+                }}
+            />
+            <Drawer.Screen
+                name="schedule/index" // This is the name of the page and must match the url from root
+                options={{
+                    drawerLabel: "Horario",
+                    title: "Horario",
+                    drawerIcon: ({ color, size }) => {
+                        <Ionicons name="calendar-outline"
+                            size={size}
+                            color={color}
+
+                        />;
+                    },
+                }}
+            />
+        </Drawer>
+    );
 };
 
 export default DrawerLayout;
