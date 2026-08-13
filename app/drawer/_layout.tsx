777421f3ce560b29bde73e2bfdbac50cabf46893
Milton Munoz <livemilton@gmail.com>
2026-08-12 21:37:12 -0500

    feature((HU-02)-HU(HU-02): :sparkles: React native - Drawer - drawerContent

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Drawer } from "expo-router/drawer";
 import { Ionicons } from "@expo/vector-icons";
+import { Text } from "react-native";
+import CustomDrawer from "@/components/shared/CustomDrawer";
 
 const DrawerLayout = () => {
     return (
         <Drawer
+            drawerContent={CustomDrawer}
             screenOptions={{
                 overlayColor: "rgba(0,0,0,0.4)",
                 drawerActiveTintColor: "indigo",
