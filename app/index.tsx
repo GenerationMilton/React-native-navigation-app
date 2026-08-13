1ea9c738c35152e80624d84967ec3477208a51ce
Milton Munoz <livemilton@gmail.com>
2026-08-12 19:26:56 -0500

    feature((HU-02)-HU(HU-02): :sparkles: React native - Tabs

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,8 @@ import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
-  return <Redirect href="/(stack)/home" />;
+  // return <Redirect href="/(stack)/home" />;
+  return <Redirect href="/tabs" />;
 
   // {
   /* // return (
